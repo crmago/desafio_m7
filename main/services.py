f080044d7648c0e82c70719346394c32a70b0ab8
Cristian Mancilla <crmago@gmail.com>
2024-07-09 19:30:46 -0400

    Inicializando proyecto en el repositorio

---
+++ main/services.py
@@ -0,0 +1,15 @@
+def crear_inmueble(*args):
+  pass
+
+def editar_inmueble(*args):
+  pass
+
+def eliminar_inmueble(inmueble_id):
+  pass
+
+def crear_user(*args):
+  # viene directo de OnlyFlans
+  pass
+
+def editar_user(*args):
+  pass
