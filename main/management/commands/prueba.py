5b422f5be9258e4f2fdfb8796800c3fbe4a15724
Cristian Mancilla <crmago@gmail.com>
2024-07-09 21:37:19 -0400

    agregamos services.py y prueba.py

---
+++ main/management/commands/prueba.py
@@ -0,0 +1,11 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+from main.services import *
+
+class Command(BaseCommand):
+  def handle(self, *args, **kwargs):
+
+    crear_user('2345678-9', 'Pablo', 'Marmol', 'marmolp@gmail.com', '67890', '67890', 'Av.piedradura 789')
+    #editar_user('1234567-8', 'Pedro', 'Picapiedras', 'pedrop@gmail.com', '54321', 'Av.Piedradura 45')
+    #editar_user('1234567-8', 'Pedro', 'Picapiedras', 'picapiedra@gmail.com', '12354', 'Av.Piedradura 45')
+    editar_user('2345678-9', 'Pablo', 'Marmol', 'pablomarmol@gmail.com', '97680', 'Av.Rocadura 567')
--- main/services.py
+++ main/services.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
+from main.models import UserProfile
+from django.db.utils import IntegrityError
+
+
+
 def crear_inmueble(*args):
   pass
 
@@ -7,9 +13,34 @@ def editar_inmueble(*args):
 def eliminar_inmueble(inmueble_id):
   pass
 
-def crear_user(*args):
-  # viene directo de OnlyFlans
-  pass
+def crear_user(username, first_name, last_name, email, password, pass_confirm, direccion, telefono=None) -> list[bool, str]:
+  # 1. validamos que los password coincidan
+  if password != pass_confirm:
+    return False, 'Las contraseñas no coinciden'
+  # 2. creamos el objeto user
+  try:
+    user = User.objects.create_user(username, email, password, first_name=first_name, last_name=last_name)
+  except IntegrityError:
+    # se le da feedback al usuario
+    return False, 'RUT Duplicado'
+  # 3. creamos el userprofile
+  UserProfile.objects.create(user=user, direccion=direccion, telefono=telefono)
+  # 4. si todo sale bien retornamos true
+  return True, None
 
-def editar_user(*args):
-  pass
+def editar_user(username, first_name, last_name, email, password, direccion, telefono=None) -> list[bool, str]:
+  # 1. Nos traemos el 'user' y modificamos sus datos
+  user = User.objects.get(username=username)
+  user.first_name = first_name
+  user.last_name = last_name
+  user.email = email
+  user.set_password(password)
+  user.save()
+  # 2. Nos traemos el 'userprofile' y modificamos sus datos
+  userprofile = UserProfile.objects.get(user=user)
+  userprofile.direccion = direccion
+  userprofile.telefono = telefono
+  userprofile.save()
+
+
+  
